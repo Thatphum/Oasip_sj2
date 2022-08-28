e23b7258638fa8f378abcff62200694539521cb2
Thatphum.pk <thatphum.pk@gmail.com>
2022-08-29 02:11:03 +0700

    modified Backend and frontend

--- Database/oasip-sj-2-structures-and-data.sql
+++ Database/oasip-sj-2-structures-and-data.sql
@@ -105,6 +105,7 @@ CREATE TABLE `users` (
   `userId` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `role` ENUM('admin', 'lecturer', 'student') NOT NULL,
   `createOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updateOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -120,7 +121,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (userId,name,email,role) VALUES (1,'OASIP ADMIN','oasip.admin@kmutt.ac.th','admin'),(2,'Somchai Jaidee','somchai.jai@kmutt.ac.th','lecturer'),(3,'Komkrid Rakdee','komkrid.rak@mail.kmutt.ac.th','student'),(4,'สมเกียรติ ขยันเรียน','somkiat.kay@kmutt.ac.th','student');
+-- INSERT INTO `users` (userId,name,email,role) VALUES (1,'OASIP ADMIN','oasip.admin@kmutt.ac.th','admin'),(2,'Somchai Jaidee','somchai.jai@kmutt.ac.th','lecturer'),(3,'Komkrid Rakdee','komkrid.rak@mail.kmutt.ac.th','student'),(4,'สมเกียรติ ขยันเรียน','somkiat.kay@kmutt.ac.th','student');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
--- Frontend/src/views/SignUp.vue
+++ Frontend/src/views/SignUp.vue
@@ -6,7 +6,6 @@ onBeforeMount(async () => {
   // console.log(users.value);
 });
 
-const users = ref();
 // const getAllUser = async () => {
 //   const res = await UserDataService.retrieveAllUser();
 //   users.value = await res.json();
@@ -14,15 +13,19 @@ const users = ref();
 
 const userName = ref('');
 const userEmail = ref('');
+const userPassword = ref('');
 const userRole = ref('');
 
 const errorName = ref();
 const errorEmail = ref();
 
+// const checkEmail =
+
 const submitUser = async () => {
   var newUser = {
     name: userName.value.trim(),
     email: userEmail.value.trim(),
+    password: password.value,
     role: userRole.value,
   };
   console.log(newUser);
@@ -34,19 +37,37 @@ const submitUser = async () => {
   }
 };
 
-const EmailTrim = () => {
-  userEmail.value = userEmail.value.trim();
-};
-
 const reset = () => {
   userName.value = '';
   userEmail.value = '';
   userRole.value = '';
+  userPassword.value = '';
+  password.value = '';
+  confirm_pass.value = '';
+  error_message.value = false;
+};
+
+const password = ref('');
+const confirm_pass = ref('');
+const error_message = ref(false);
+
+const EmailTrim = () => {
+  userEmail.value = userEmail.value.trim();
+};
+
+const checkPassword = () => {
+  if (password.value != '' && confirm_pass.value != '') {
+    if (password.value != confirm_pass.value) {
+      error_message.value = true;
+    } else {
+      error_message.value = false;
+    }
+  }
 };
 </script>
 
 <template>
-  <div class="bg-grey-lighter mt-52 flex flex-col">
+  <div class="bg-grey-lighter mt-24 flex flex-col">
     <form
       class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
       @submit="submitUser()"
@@ -54,18 +75,22 @@ const reset = () => {
     >
       <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
         <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+        <label for="nameform">Name</label>
         <input
           required
+          id="nameform"
           type="text"
           class="block border border-grey-light w-full p-3 rounded mb-4"
           name="name"
           maxlength="100"
           v-model="userName"
-          placeholder="Username"
+          placeholder="E.g. John Washington"
         />
         <span class="text-red-500 mt-2">{{ errorName }}</span>
+        <label for="emailform">Email</label>
         <input
           required
+          id="emailform"
           type="text"
           class="block border border-grey-light w-full p-3 rounded mb-4"
           name="email"
@@ -73,21 +98,43 @@ const reset = () => {
           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
           maxlength="50"
           v-model="userEmail"
-          placeholder="Email"
+          placeholder="john@example.com"
         />
         <span class="text-red-500 mt-2">{{ errorEmail }}</span>
-        <!-- <input
+        <label for="pwdform">Password</label>
+        <input
+          id="pwdform"
           type="password"
           class="block border border-grey-light w-full p-3 rounded mb-4"
           name="password"
-          placeholder="Password"
-        /> -->
-        <!-- <input
+          pattern=".{8,15}"
+          placeholder="password"
+          v-model="password"
+          @change="checkPassword()"
+        />
+        <label for="checkpwd">Confirm Password</label>
+        <input
+          id="checkpwd"
           type="password"
           class="block border border-grey-light w-full p-3 rounded mb-4"
           name="confirm_password"
           placeholder="Confirm Password"
-        /> -->
+          pattern=".{8,15}"
+          v-model="confirm_pass"
+          @change="checkPassword()"
+        />
+
+        <span v-show="error_message" class="text-red-600"
+          >The password DOES NOT match</span
+        >
+        <span
+          v-show="
+            (password.length < 8 || password.length > 14) && password != ''
+          "
+          class="text-red-600"
+          >between 8 - 14 charaters</span
+        >
+
         <div class="mb-6">
           <label
             for="category"
