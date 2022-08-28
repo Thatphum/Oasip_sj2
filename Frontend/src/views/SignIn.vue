1085e3beab596823325c99eb5da5cb95ee75b47e
neko7687 <neko7687@gmail.com>
2022-08-28 23:06:43 +0700

    SignIn

---
+++ Frontend/src/views/SignIn.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-grey-lighter min-h-screen flex flex-col">
+    <div
+      class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+    >
+      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <h1 class="mb-8 text-3xl text-center">Sign In</h1>
+        <input
+          type="text"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="email"
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="password"
+          placeholder="Password"
+        />
+
+        <button
+          type="submit"
+          class="w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1"
+        >
+          Log In
+        </button>
+      </div>
+
+      <div class="text-grey-dark mt-6">
+        Don't have an account?
+        <router-link
+            class="no-underline border-b border-blue text-blue"
+            :to="{ path: '/signUp', name: 'SignUp' }"
+            > Create yours now. </router-link
+          >.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
