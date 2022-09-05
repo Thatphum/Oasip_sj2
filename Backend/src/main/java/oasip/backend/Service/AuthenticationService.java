4f9faa7217b37bbb2d3c559813997d3c2f6ce987
Thatphum.pk <thatphum.pk@gmail.com>
2022-09-06 02:39:56 +0700

    merge backend token

--- Backend/src/main/java/oasip/backend/Service/AuthenticationService.java
+++ Backend/src/main/java/oasip/backend/Service/AuthenticationService.java
@@ -1,59 +1,80 @@
 package oasip.backend.Service;
 
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
+import oasip.backend.Config.JwtTokenUtil;
+import oasip.backend.Config.Jwts.AuthenticationUser;
+import oasip.backend.Config.Jwts.JwtResponse;
+import oasip.backend.Config.Jwts.JwtUserDetailsService;
 import oasip.backend.DTOs.Authentication.LoginDTO;
-import oasip.backend.DTOs.User.UserListAllDto;
+import oasip.backend.DTOs.Authentication.TokenDto;
 import oasip.backend.Enitities.User;
 import oasip.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-
 @Service
 public class AuthenticationService {
+
     @Autowired
-    UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    JwtUserDetailsService jwtUserDetailService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
 
 //    private Argon2PasswordEncoder passwordEncoder = new Argon2PasswordEncoder(16, 26, 1, 65536, 10);
     private Argon2PasswordEncoder passwordEncoder = new Argon2PasswordEncoder();
 
-    public boolean match(LoginDTO oldUser) {
-        User user = userRepository.findByEmail(oldUser.getEmail());
-        if(user == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A user with the specified email DOES NOT exist");
-        }
-        if(!(passwordEncoder.matches(oldUser.getPassword(),user.getPassword()))){
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Password NOT Matched");
-        }
-        return true;
+    public TokenDto match(LoginDTO oldUser) {
+//        try {
+            User user = userRepository.findByEmail(oldUser.getEmail());
+//            System.out.println(user);
+//            authenticate(oldUser.getEmail(), oldUser.getPassword())
+            if (user != null) {
+                final AuthenticationUser authenticationUser = jwtUserDetailService
+                        .loadUserByUsername(oldUser.getEmail());
+                final String token = jwtTokenUtil.generateToken(authenticationUser);
+//                System.out.println("username : " + authenticationUser.getUsername() + "password : " + authenticationUser.getPassword());
+                if(!(passwordEncoder.matches(oldUser.getPassword(),user.getPassword()))){
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Password NOT Matched");
+                }
+                JwtResponse<?> tokens = new JwtResponse<User>(token, user, authenticationUser.getAuthorities());
+                TokenDto tokenDto = new TokenDto();
+                tokenDto.setToken(tokens.getToken());
+                return tokenDto;
+            } else {
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error("Unable to generate token !"));
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A user with the specified email DOES NOT exist");
+            }
+
+//        } catch (Exception e) {
+//            new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+////            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error("kuy", e.getCause()));
+//        }
+    }
+    private void authenticate(String username, String password) throws Exception {
+//        try {
+//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+//        } catch (DisabledException e) {
+//            throw new Exception("USER_DISABLED", e);
+//        } catch (BadCredentialsException e) {
+//            throw new Exception("INVALID_CREDENTIALS", e);
+//        }
+
     }
 
-//    public boolean match() {
-//        Argon2 argon2 = Argon2Factory.create(
-//                Argon2Factory.Argon2Types.ARGON2id,
-//                10,
-//                32);
-//        String hash = argon2.hash(30, 65536, 1,"password");
-////        System.out.println(hash);
-////        Argon2PasswordEncoder passwordEncoder = new Argon2PasswordEncoder();
-////        String s = passwordEncoder.encode("password");
-//        boolean success = argon2.verify(hash, "password");
-////        System.out.println(success);
-//
-////        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        Argon2PasswordEncoder encoder = new Argon2PasswordEncoder(16, 26, 1, 65536, 10);
-//        String result = encoder.encode("ram123sdadsadsadsa");
-//        System.out.println(result);
-//        System.out.println(encoder.matches("ram123", result));
-////        System.out.println(result);
-//        return true;
-//    }
+
+
 }
