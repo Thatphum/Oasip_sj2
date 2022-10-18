e4f44d4fdab9b5a99634e83d153057fb6ee722f4
Thatphum.pk <thatphum.pk@gmail.com>
2022-10-18 15:14:17 +0700

    @Bean     public HttpFirewall allowUrlSemicolonHttpFirewall() {         StrictHttpFirewall firewall = new StrictHttpFirewall();         firewall.setAllowSemicolon(true);         return firewall;     }

--- Backend/src/main/java/oasip/backend/BackendApplication.java
+++ Backend/src/main/java/oasip/backend/BackendApplication.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 import java.util.Date;
 
@@ -17,4 +20,11 @@ public class BackendApplication {
 
     }
 
+
+    @Bean
+    public HttpFirewall allowUrlSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
+    }
 }
