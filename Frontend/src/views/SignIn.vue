27cb88d13a76b25b8143070dc19d42c27e3c7086
neko7687 <neko7687@gmail.com>
2022-11-17 19:57:58 +0700

    Update Homepage

--- Frontend/src/views/SignIn.vue
+++ Frontend/src/views/SignIn.vue
@@ -4,21 +4,21 @@ import LoginService from '../services/AuthenticationService';
 const email = ref('');
 const password = ref('');
 
-const submitLogin = async () => {
-  var requiredUser = {
-    email: email.value.trim(),
-    password: password.value,
-  };
-  console.log(requiredUser);
-  const res = await LoginService.logInUser(requiredUser);
-  if (res.status == 400) {
-    alert('A user with the specified email DOES NOT exist');
-  } else if (res.status == 401) {
-    alert('Password NOT Matched');
-  } else {
-    alert('Password Matched');
-  }
-};
+// const submitLogin = async () => {
+//   var requiredUser = {
+//     email: email.value.trim(),
+//     password: password.value,
+//   };
+//   console.log(requiredUser);
+//   const res = await LoginService.logInUser(requiredUser);
+//   if (res.status == 400) {
+//     alert('A user with the specified email DOES NOT exist');
+//   } else if (res.status == 401) {
+//     alert('Password NOT Matched');
+//   } else {
+//     alert('Password Matched');
+//   }
+// };
 
 const EmailTrim = () => {
   email.value = email.value.trim();
@@ -26,51 +26,70 @@ const EmailTrim = () => {
 </script>
 
 <template>
-  <div class="bg-grey-lighter min-h-screen flex flex-col">
-    <form
-      class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
-      @submit="submitLogin()"
-      onsubmit="return false;"
+  <section class="grid place-items-center h-screen bg-slate-100">
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 max-h-[65%] max-w-[75%] md:max-w-[80%] lg:max-w-[50%] rounded-lg bg-white drop-shadow-2xl divide-x"
     >
-      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-        <h1 class="mb-8 text-3xl text-center">Sign In</h1>
-        <input
-          type="text"
-          class="block border border-grey-light w-full p-3 rounded mb-4"
-          name="email"
-          placeholder="Email"
-          @change="EmailTrim()"
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
-          v-model="email"
-        />
-
-        <input
-          type="password"
-          class="block border border-grey-light w-full p-3 rounded mb-4"
-          name="password"
-          placeholder="Password"
-          v-model="password"
-        />
-
-        <button
-          type="submit"
-          class="w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1"
-        >
-          Log In
-        </button>
+      <div class="rounded-l-lg p-10">
+        <img src="../assets/bgCreate.png" alt="" />
       </div>
-
-      <div class="text-grey-dark mt-6">
-        Don't have an account?
-        <router-link
-          class="no-underline border-b border-blue text-blue"
-          :to="{ path: '/signUp', name: 'SignUp' }"
+      <div class="flex flex-col justify-center items-center">
+        <h1 class="text-center text-4xl pt-7">Sign In</h1>
+        <form
+          class="mx-auto flex-1 flex flex-col items-center justify-center px-2"
+          @submit="submitLogin()"
+          onsubmit="return false"
         >
-          Create yours now. </router-link
-        >.
+          <div class="text-black w-full">
+            <h3 class="text-xl">Email</h3>
+            <input
+              type="text"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="email"
+              placeholder="Email"
+              @change="EmailTrim()"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
+              v-model="email"
+            />
+            <h3 class="text-xl">Password</h3>
+            <input
+              type="password"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="password"
+              placeholder="Password"
+              v-model="password"
+            />
+            <div class="flex">
+              <button
+                type="submit"
+                class="w-20 text-center py-3 rounded bg-blue-700 text-white hover:bg-blue-800 focus:outline-none my-1 border-solid-black"
+              >
+                Sign In
+              </button>
+              <router-link :to="{ path: '/', name: 'Home' }"
+                ><button
+                  type="cancel"
+                  class="w-20 text-center py-3 rounded bg-white text-black hover:bg-slate-300 focus:outline-none my-1 ml-5"
+                  @click="cancel()"
+                >
+                  Cancel
+                </button></router-link
+              >
+            </div>
+            <div class="text-grey-dark mt-6">
+              Don't have an account?
+              <router-link
+                class="underline text-blue"
+                :to="{ path: '/signUp', name: 'SignUp' }"
+              >
+                Create your account?
+              </router-link>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
+    </div>
+  </section>
 </template>
 
 <style></style>
