9da2fa102db4137b00ad3259bf106bd44f4dafef
Preeyathorn Chaiyakum <neko7687@gmail.com>
2022-12-19 09:23:23 +0700

    Footer

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -15,10 +15,12 @@
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 export default {
   name: 'App',
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
