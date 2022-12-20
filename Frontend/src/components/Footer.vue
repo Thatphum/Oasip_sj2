d3fa5e2360f827d7ccfaae89d5c9eb4637c0cab7
Thatphum.pk <thatphum.pk@gmail.com>
2022-12-20 13:36:56 +0700

    edit footer

--- Frontend/src/components/Footer.vue
+++ Frontend/src/components/Footer.vue
@@ -9,17 +9,23 @@
       >
         <div class="text-2xl text-center">Jaturalnw</div>
         <ul
-          class="flex justify-center gap-1 items-center mt-2 md:mt-0 text-center md:text-white text-gray-300"
+          class="flex justify-center gap-2 items-center mt-2 md:mt-0 text-center md:text-white text-gray-300"
         >
-          <li>Home</li>
+          <!-- <li>Home</li>
           <li class="md:hidden block">|</li>
           <li>Events</li>
           <li class="md:hidden block">|</li>
           <li>Categories</li>
           <li class="md:hidden block">|</li>
           <li>Users</li>
-          <li class="md:hidden block">|</li>
-          <li>Contact us</li>
+          <li class="md:hidden block">|</li> -->
+
+          <li>
+            <router-link :to="{ name: 'Home' }"> Home </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'About' }"> Contact Us </router-link>
+          </li>
         </ul>
       </div>
       <div class="flex justify-center p-2 border-t text-center break-word">
